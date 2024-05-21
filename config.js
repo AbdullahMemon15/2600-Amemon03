ec2afbe671be8ee2647713dac67a12c7cd26883a
Abdullah Memon <markevanshero@gmail.com>
2024-05-20 23:53:32 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,16 @@
 const mongoose = require("mongoose");
 require('dotenv').config();
 
-let mongoDB = process.env.DB_CONNECTION;
+// let mongoDB = process.env.DB_CONNECTION;
+
+const uri = process.env.MONGODB_URI;
+
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.log('MongoDB connection error: ', err));
+
 
 module.exports = mongoose.connect(mongoDB);
