a02186f6554cd6c64b6c0b372e7ad56a583f46f7
Abdullah Memon <markevanshero@gmail.com>
2024-05-24 01:18:22 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,6 @@
 const mongoose = require("mongoose");
 require('dotenv').config();
 
-console.log('DB_CONNECTION:', process.env.DB_CONNECTION); // Log to ensure it's loaded
-
 let mongoDB = process.env.DB_CONNECTION;
 
-mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('MongoDB connected'))
-.catch(err => {
-  console.error('MongoDB connection error: ', err.message);
-});
-
-module.exports = mongoose;
+module.exports = mongoose.connect(mongoDB);
