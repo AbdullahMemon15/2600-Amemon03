21e54c8af4622efb9e2f9ab6421fe258e236262a
Abdullah Memon <markevanshero@gmail.com>
2024-05-24 01:37:52 -0700

    Update app.js

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const connection  = require("./config");
 const prayerRoutes = require("./routes/prayerRoutes");
-const cors = require('cors');
+// const cors = require('cors');
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors()); // Use cors middleware
+// app.use(cors()); // Use cors middleware
 app.use(express.static('public'));
 app.use(express.urlencoded({extended:true}));
 
